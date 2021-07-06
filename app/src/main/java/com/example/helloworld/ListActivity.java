78351736422c5a0d491ee38745d1acaac311c344
ward awiedah <78215331+Ward-aweida@users.noreply.github.com>
2021-07-06 16:54:15 +0300

    15

--- app/src/main/java/com/example/helloworld/ListActivity.java
+++ app/src/main/java/com/example/helloworld/ListActivity.java
@@ -3,9 +3,11 @@ package com.example.helloworld;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -25,6 +27,19 @@ EditText textViewShared;
         listAdd = findViewById(R.id.ListActivity);
         preferences = getSharedPreferences("SHERD_PREF",MODE_PRIVATE);
         String  Name = preferences.getString("Name","");
-        
+
+
+
+        listAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (textViewsh.getText().toString().trim().length() <= 0) {
+                    Toast.makeText(ListActivity.this, "It's empty", Toast.LENGTH_SHORT).show();
+                }
+
+            }
+
+
+        });
     }
 }
